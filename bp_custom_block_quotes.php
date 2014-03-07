fbf83462d838af0ce652dad9b600c8eae69251f1
Diane Ensey <diane@beyond-paper.com>
2014-03-07 12:11:09 -0800

    Initial commit of v1 with updated style leather and removal of <blockquote> tags.

---
+++ bp_custom_block_quotes.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Plugin Name: Customized Blockquotes
+Plugin URI: www.beyond-paper.com
+Description: Replaces a shortcode with a formatted blockquote.  Use the following format: [quote image="theimage.jpg" title="the title" source="the_source.html" author="The Author"]The Quotation Body[/quote]  Original CSS by CoDrops (http://tympanus.net/codrops/2012/07/25/modern-block-quote-styles/)
+Version: 1.0
+Author: Diane Ensey
+Author URI: www.beyond-paper.com
+/*/
+
+
+/**
+ * Customized Blockquotes is a plugin for WordPress 3.0.5
+ * It was created by Diane Ensey (diane@beyond-paper.com.com) using CSS by CoDrops (http://tympanus.net/codrops/2012/07/25/modern-block-quote-styles/)
+ * Free for personal use. Any distribution, copy, resell, rent or related action is allowed only you use a backlink to http://www.beyond-paper.com
+**/
+
+
+/**
+ * 1. WP path definitions ----------------------------------------------------------------->
+**/
+
+if(!defined('WP_CONTENT_URL'))
+	define('WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
+if(!defined('WP_CONTENT_DIR'))
+	define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
+if(!defined('WP_PLUGIN_URL'))
+	define('WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins');
+if(!defined('WP_PLUGIN_DIR'))
+	define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins');
+
+
+/**
+ * 2. CSS and JS definitions ----------------------------------------------------------------->
+**/
+class BPcb {
+    function bpIncludes() {
+      $cssPath = WP_PLUGIN_URL.'/'.plugin_basename(dirname(__FILE__)).'/styles/'.'/';
+	  //$jsPath = WP_PLUGIN_URL.'/'.plugin_basename(dirname(__FILE__)).'/js/'.'/';
+
+      echo '<link rel="stylesheet" type="text/css" media="screen" href="' . $cssPath . 'bp_style.css" />'."\n";
+	
+    }
+}
+
+
+/**
+ * 3. Functions ----------------------------------------------------------------->
+**/
+
+/** ---------- Custom functions START ---------- **/
+
+
+/*Add the Shortcode 
+** Format is 
+** [quote style = 'leather' image="theimage.jpg" title="the title" source="the_source.html" author="The Author"]The Quotation Body[/quote]
+** styles:
+** 		leather
+** 
+** 
+** 
+** 
+*/
+function bp_custom_block_quote($atts, $content = null) {
+   extract(shortcode_atts(array(
+   	  "style"  => 'leather',
+      "image"  => ' ',
+      "title"  => 'no title provided',
+      "source" => ' ',
+      "author" => 'Unknown'
+   ), $atts));
+   return '<div class="bp-wrap '.$style.'">
+		<div class="bp-thumb" style="background-image:url('.$image.')"></div>
+		<div class="blockquote" cite="'.$source.'"><p>'.$content.'</p></div>
+		<div class="bp-attribution">
+		<p class="bp-author">'.$author.'</p>
+		<p><cite><a href="'.$source.'">'.$title.'</a></cite></p>
+		</div>
+		</div>';
+}
+
+/** ---------- Custom functions END ---------- **/
+
+/**
+ * 4. Actions ----------------------------------------------------------------->
+**/
+
+if (class_exists("BPcb")) {
+	$dl_plugin = new BPcb();
+}
+//Actions
+if (isset($dl_plugin)) {
+	//Add Action To Footer
+	add_action('wp_footer', array(&$dl_plugin, 'bpIncludes')); //Add the CSS and JS definitions to the footer section
+}
+
+/**
+ * 5. Shortcodes ----------------------------------------------------------------->
+**/
+
+add_shortcode("quote", "bp_custom_block_quote");
+?>
